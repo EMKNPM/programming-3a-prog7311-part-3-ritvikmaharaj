ad0aece082467a3ca89da99c9bd38850e86b5fb2
Ritvik Maharaj <ST10438117@vcconnect.edu.za>
2026-06-06 10:05:54 +0200

    API and tests

--- GLMS.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GLMS.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
